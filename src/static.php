316d547fb80c8141dafeedc215d09bb6fb78ffe9
David Vielhuber <david@vielhuber.de>
2017-07-18 17:23:30 +0200

    modified wordpress version

--- src/static.php
+++ src/static.php
@@ -1,5 +1,5 @@
 <?php
 function db_fetch_var(...$query) { global $db; return $db->fetch_var(...$query); }
-function db_fetch_row(...$query) { global $db; return $db->fetch_var(...$query); }
-function db_fetch_col(...$query) { global $db; return $db->fetch_var(...$query); }
-function db_fetch_all(...$query) { global $db; return $db->fetch_var(...$query); }
+function db_fetch_row(...$query) { global $db; return $db->fetch_row(...$query); }
+function db_fetch_col(...$query) { global $db; return $db->fetch_col(...$query); }
+function db_fetch_all(...$query) { global $db; return $db->fetch_all(...$query); }
