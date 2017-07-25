592e29acb4eece6cf83a263754d35d81003b1f50
David Vielhuber <david@vielhuber.de>
2017-07-25 17:06:21 +0200

    small glitch with ctype_digit

--- src/dbhelper.php
+++ src/dbhelper.php
@@ -485,7 +485,7 @@ class dbhelper
             {
                 // replace first occurence
                 $directive = '%s';
-                if( is_int($params__value) || (is_string($params__value) && ctype_digit($params__value)) ) { $directive = '%d'; }
+                if( is_int($params__value) || (string)ctype_digit($params__value) ) { $directive = '%d'; }
                 else if( is_float($params__value) ) { $directive = '%f'; }                
                 $return = substr_replace($return, $directive, strpos($return, '?'), strlen('?'));
             }
