cf1bc6e771306e40cbad65ffcebcc00e0b203ffc
David Vielhuber <david@vielhuber.de>
2017-11-07 16:13:37 +0100

    added more static functions

--- src/static.php
+++ src/static.php
@@ -4,3 +4,7 @@ function db_fetch_row(...$query) { global $db; return $db->fetch_row(...$query);
 function db_fetch_col(...$query) { global $db; return $db->fetch_col(...$query); }
 function db_fetch_all(...$query) { global $db; return $db->fetch_all(...$query); }
 function db_query(...$query) { global $db; return $db->query(...$query); }
+function db_insert($table, $data) { global $db; return $db->insert($table, $data); }
+function db_update($table, $data, $condition = null) { global $db; return $db->update($table, $data, $condition); }
+function db_delete($table, $conditions) { global $db; return $db->delete($table, $conditions); }
+function db_update_batch($table, $input) { global $db; return $db->update_batch($table, $input); }
