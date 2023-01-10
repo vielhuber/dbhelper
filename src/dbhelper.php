a7f62a58b54bcf63f16937e85258cfb7016d9a09
David Vielhuber <david@vielhuber.de>
2023-01-10 11:53:41 +0100

    Add github actions.

--- src/dbhelper.php
+++ src/dbhelper.php
@@ -29,7 +29,11 @@ class dbhelper
             case 'pdo':
                 if ($engine === 'mysql') {
                     $sql = new PDO(
-                        'mysql:host=' . $host . ';port=' . $port . ($database !== null ? ';dbname=' . $database : ';charset=utf8mb4'),
+                        'mysql:host=' .
+                            $host .
+                            ';port=' .
+                            $port .
+                            ($database !== null ? ';dbname=' . $database : ';charset=utf8mb4'),
                         $username,
                         $password,
                         [
@@ -1260,10 +1264,16 @@ class dbhelper
             $$ language \'plpgsql\';
         ');
         $this->query(
-            'DROP TRIGGER IF EXISTS ' . $this->quote('auto_update_updated_at_column_on_insert') . ' ON ' . $this->config['logging_table']
+            'DROP TRIGGER IF EXISTS ' .
+                $this->quote('auto_update_updated_at_column_on_insert') .
+                ' ON ' .
+                $this->config['logging_table']
         );
         $this->query(
-            'DROP TRIGGER IF EXISTS ' . $this->quote('auto_update_updated_at_column_on_update') . ' ON ' . $this->config['logging_table']
+            'DROP TRIGGER IF EXISTS ' .
+                $this->quote('auto_update_updated_at_column_on_update') .
+                ' ON ' .
+                $this->config['logging_table']
         );
         $this->query(
             'CREATE TRIGGER auto_update_updated_at_column_on_insert BEFORE INSERT ON ' .
